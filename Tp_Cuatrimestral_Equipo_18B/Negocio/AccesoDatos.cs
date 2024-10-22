c1a7a7e1637a82b0fef89cd2723565ad50ace0ef
Nacho-Aramburu <nachoaramburu29@gmail.com>
2024-10-22 20:29:40 -0300

    Acceso a Base De Datos

--- Tp_Cuatrimestral_Equipo_18B/Negocio/AccesoDatos.cs
+++ Tp_Cuatrimestral_Equipo_18B/Negocio/AccesoDatos.cs
@@ -1,12 +1,89 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dominio;
 
 namespace Negocio
 {
-    internal class AccesoDatos
+    public class AccesoDatos
     {
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+        public SqlDataReader Lector
+        {
+            get { return lector; }
+        }
+        public AccesoDatos()
+        {
+            conexion = new SqlConnection("server=.\\SQLEXPRESS; database=SEGURIDAD_DB; integrated security = true");
+            comando = new SqlCommand();
+
+        }
+        public void setearConsulta(string consulta)
+        {
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+        }
+        public void ejecutarConsulta()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                lector = comando.ExecuteReader();
+
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+
+        }
+        public void cerrarConexion()
+        {
+            if (lector != null)
+            {
+                lector.Close();
+            }
+
+            conexion.Close();
+        }
+
+        public void setearParametro(string nombre, object valor)
+        {
+            comando.Parameters.AddWithValue(nombre, valor);
+        }
+        public bool EjecutarAccion()
+        {
+            bool response = false;
+
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                int filasAfectadas = comando.ExecuteNonQuery();
+
+                if (filasAfectadas > 0)
+                    response = true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return response;
+        }
+        public void Cerrar()
+        {
+            if (lector != null)
+                lector.Close();
+
+            conexion.Close();
+        }
+
     }
 }
