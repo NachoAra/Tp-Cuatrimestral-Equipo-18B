d87ff539f09d870c66f66ba1b5a66c19263025b0
Nacho-Aramburu <nachoaramburu29@gmail.com>
2024-10-22 21:01:28 -0300

    Lectura de BDD con entidad cliente

--- Tp_Cuatrimestral_Equipo_18B/Negocio/ClienteNegocio.cs
+++ Tp_Cuatrimestral_Equipo_18B/Negocio/ClienteNegocio.cs
@@ -9,5 +9,82 @@ namespace Negocio
 {
     public class ClienteNegocio
     {
+        private AccesoDatos accesoDatos;
+        public ClienteNegocio()
+        {
+            accesoDatos = new AccesoDatos();
+        }
+        public bool ExisteDNI(string NroCliente)
+        {
+            try
+            {
+                accesoDatos.setearConsulta("SELECT NroCliente FROM Clientes WHERE NroCliente = @nroCliente");
+                accesoDatos.setearParametro("@nroCliente", NroCliente);
+                accesoDatos.ejecutarConsulta();
+
+                if (accesoDatos.Lector.Read())
+                    return true;
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                accesoDatos.cerrarConexion();
+            }
+            return false;
+        }
+
+        public Cliente GetCliente(string NroCliente)
+        {
+            Cliente cliente = new Cliente();
+            try
+            {
+                accesoDatos.setearConsulta("SELECT * FROM Clientes WHERE NroCliente = @nroCliente");
+                accesoDatos.setearParametro("@nroCliente", NroCliente);
+                accesoDatos.ejecutarConsulta();
+
+                if (accesoDatos.Lector.Read())
+                {
+                    cliente.NroCliente = NroCliente;
+                    
+                }
+
+                return cliente;
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                accesoDatos.cerrarConexion();
+            }
+        }
+
+        public bool RegistrarCliente(Cliente cliente)
+        {
+            bool response = false;
+
+            try
+            {
+                AccesoDatos accesoDatos = new AccesoDatos();
+                accesoDatos.setearConsulta("INSERT INTO Clientes (NroCliente)VALUES(@nroCliente)");
+                
+                if (accesoDatos.EjecutarAccion())
+                    response = true; ;
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                accesoDatos.cerrarConexion();
+            }
+            return response;
+        }
     }
 }
+
